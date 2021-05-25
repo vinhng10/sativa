f84f587ae766a297a8ac83cbea8fd9b7d33e8b2f
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-05-25 10:15:47 +0300

    add: add script for loading environment variables

---
+++ core/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+## Install packages:
+echo "Installing packages ..."
+apt-get update
+apt-get install python-pip
+pip install python-swiftclient
+pip install python-keystoneclient
+apt-get install s3cmd
+curl https://rclone.org/install.sh | bash
+
+
+## Load environment variables:
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+
+## Declare target bucket name for tool experiments:
+readonly SWIFT="vinh-swift-experiemnts"
+#readonly S3CMD="vinh-s3cmd-experiments"
+#readonly RCLONE="vinh-rclone-experiements"
+
+
