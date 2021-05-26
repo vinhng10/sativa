568b2d135d79f08ed1f873c78d3e034e81459005
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-05-26 22:13:23 +0300

    add: add some experiment codes

--- core/run.sh
+++ core/run.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-## Install packages:
-echo "Installing packages ..."
-apt-get update
-apt-get install python-pip
-pip install python-swiftclient
-apt-get install s3cmd
-curl https://rclone.org/install.sh | bash
+EXPERIMENT_NUMBER="0.0.0"
+CONTAINER="vinh-swift-experiemnts"
+FILE_PATH="./experiment_data/file_1T.h5"
+SEGMENT_SIZE="5G"
+THREAD=()
+FILE_SPLIT_SIZE=()
 
+extract_result () {
 
+}
+
+save_result () {
+    sqlite3 ./db/experiments_dev.db "INSERT INTO Experiment VALUES('cac.h5','0.0.0', 'bucket', 'mahti','login','swift','100G','5G',10,NULL,NULL,'200Mb/s','1h');"
+}
+
+## Run experiments wit swift:
+for file in ./experiment_data/*
+do
+    for segment_size in "1G" "2G" "5G"
+    do
+        swift upload --use-slo --segment-size $segment_size $SWIFT $file
+        # TODO: script to extract and save experiment's metadata
+    done
+done
+
+#if [ -f ./db/experiments_dev.db ]
+#then
+#    rm ./db/experiments_dev.db
+#fi
+#sqlite3 ./db/experiments_dev.db < ./db/create_tables.sql
+#sqlite3 ./db/experiments_dev.db "INSERT INTO File VALUES('cac.h5', '.h5', '1T');"
+#sqlite3 ./db/experiments_dev.db "INSERT INTO Experiment VALUES('cac.h5','0.0.0', 'bucket', 'mahti','login','swift','100G','5G',10,NULL,NULL,'200Mb/s','1h');"
