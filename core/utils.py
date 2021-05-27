acb3d01be55b39733bb8d46da9b8374e988b1d81
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-05-27 11:03:56 +0300

    add: add function to split file into smaller files with equal size.

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,7 @@
 import sqlite3
+import subprocess
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 AnyPath = Union[Path, str, bytes]
 
@@ -36,3 +37,68 @@ def save_to_db(db: AnyPath, table: str, *args) -> None:
         connection.rollback()
 
     connection.close()
+
+
+def split_file(file: AnyPath, file_split_size: int = 0, suffix_length: int = 4) -> List[Path]:
+    """
+    Split a file into smaller files. All the split files will locate in dedicated
+    folder to separate from other splits (from other experiments).
+
+    The name format of dedicated folder:
+        <file_name>-<file_extension>-<file_split_size>G-splits
+
+    The name format of split file:
+        <file_name>-<file_extension>-<file_split_size>G-<number>
+
+    Parameters
+    ----------
+    file: AnyPath
+        Path to the file to be split.
+    file_split_size: int
+        Size of a split file. Measured in Gigabyte.
+    suffix_length: int
+        Length of suffix for split files.
+
+    Returns
+    -------
+    split_files: List[Path]
+        List of path to split files.
+
+    """
+    if not isinstance(file_split_size, int):
+        raise TypeError(f"Non-negataive integer file_split_size expected, but got {type(file_split_size)} instead.")
+    if not isinstance(suffix_length, int):
+        raise TypeError(f"Positive integer suffix_length expected, but got {type(suffix_length)} instead.")
+    if suffix_length < 0:
+        raise ValueError(f"Positive integer suffix_length expected, but got {suffix_length} instead.")
+
+    file = Path(file)
+    prefix = f"{file.name.replace('.', '-')}-{file_split_size}G-"
+
+    # Create directory to store split files. This is for easy management:
+    parent_folder = file.parent / (prefix + "splits")
+    parent_folder.mkdir(parents=True, exist_ok=True)
+
+    if file_split_size > 0:
+        # Call linux "split" to split the file. This is for convenience and reliability.
+        # Might need check for performance compared to native python code in different settings:
+        cmd = f"split -d -a {suffix_length} -b {file_split_size}GB " \
+              f"{file.as_posix()} {parent_folder / prefix}"
+        p = subprocess.run(
+            cmd.split(),
+            capture_output=True,
+            text=True,
+            check=True
+        )
+    else:
+        # If file_split_size is 0, then don't split the file, only move to new directory:
+        file.rename(parent_folder / (prefix + "0"*suffix_length))
+
+    # Glob all paths to split files:
+    split_files = list(parent_folder.glob(f"*{prefix}*"))
+
+    return split_files
+
+
+
+
