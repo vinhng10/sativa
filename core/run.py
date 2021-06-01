3085c5d09aa7f89ad59d6e4735755542e828276d
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-06-01 09:57:12 +0300

    update: update run experiment run script

--- core/run.py
+++ core/run.py
@@ -2,10 +2,9 @@ import argparse
 import yaml
 from pathlib import Path
 
-from utils import (
-    split_file, save_to_db,
-    upload_file_swift
-)
+from experiment import Experiment
+from utils import get_named_parameters
+
 
 ##############################################################################
 ### EXPERIMENT SETUP                                                       ###
@@ -38,55 +37,20 @@ file = Path(args.file)
 ### EXPERIMENT RUN                                                         ###
 ##############################################################################
 
-# Save file information to File table:
-save_to_db(
-    args.db, "File",
-    file.name, file.suffix, file.stat().st_size
+# Get parameter iterator:
+parameters = get_named_parameters(
+    file_split_size=config.file_split_sizes,
+    segment_size=config.segment_sizes,
+    thread=config.threads,
+    core=config.cores
 )
 
-for file_split_size in args.file_split_sizes:
-    # Split file into smaller files:
-    file_splits = split_file(args.file, file_split_size)
-
-    for segment_size in args.segment_sizes:
-
-        for file_split in file_splits:
-            # Save file split information to File table. This is to
-            # reuse the result from file split upload:
-            save_to_db(
-                args.db, "File",
-                file_split.name, file_split.suffix, file_split.stat().st_size
-            )
-
-            # Perform file upload:
-            result = upload_file_swift(
-                file_split,
-                auth["st_auth_version"],
-                auth["os_username"],
-                auth["os_password"],
-                auth["os_project_name"],
-                auth["os_auth_url"],
-                config["bucket"],
-                segment_size,
-                config
-            )
-
-            # Check the upload was successful:
-            if result.returncode == 0:
-                status = "SUCCESSFUL"
-            else:
-                status = "FAILED"
-
-            transfer_rate = get_transfer_rate()
-            transfer_time = get_transfer_time()
-
-            # Save result to Experiment table:
-            save_to_db(
-                args.db, "Experiment",
-                file_split.name, config.version, config.bucket,
-                config.cluster, config.node, config.tool,
-                file_split_size, segment_size, config.thread,
-                config.core, config.process, transfer_rate,
-                transfer_time, status
-            )
-
+# Run experiment on each parameter setting:
+for parameter in parameters:
+    experiment = Experiment(
+        config.db, file, config.version, config.bucket,
+        config.cluster, config.node, config.tool,
+        parameter.file_split_size, parameter.segment_size,
+        parameter.thread, parameter.cores, auth
+    )
+    experiment.run()
