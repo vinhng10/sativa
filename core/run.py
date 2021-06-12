ff0992b84874dab7a8f2c28710fd623f16ecfba0
Vinh Nguyen <qvinhnguyen17@gmail.com>
2021-06-12 11:00:14 +0300

    fix: wrap run script in main check

--- core/run.py
+++ core/run.py
@@ -2,56 +2,64 @@ import argparse
 import json
 from pathlib import Path
 
-from experiment import Experiment
+from experiment import Experiment, SubExperiment
 from utils import get_parameters_dicts
 
 
-##############################################################################
-### EXPERIMENT SETUP                                                       ###
-##############################################################################
+if __name__ == "__main__":
+    ###########################################################################
+    ### EXPERIMENT SETUP                                                    ###
+    ###########################################################################
 
-# Parse argument:
-parser = argparse.ArgumentParser()
+    # Parse argument:
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("-f", "--file", required=True,
-                    help="Path to the file to be transfered.")
-parser.add_argument("-c", "--cfg", required=True,
-                    help="Path to the experiment config file.")
+    parser.add_argument("-f", "--file", required=True,
+                        help="Path to the file to be transfered.")
+    parser.add_argument("-c", "--cfg", required=True,
+                        help="Path to the experiment config file.")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# Read experiment config file:
-with open(args.cfg, "r") as f:
-    config = json.load(f)
+    # Read experiment config file:
+    with open(args.cfg, "r") as f:
+        config = json.load(f)
 
-# Get file path:
-file = Path(args.file)
+    # Get file path:
+    file = Path(args.file)
 
-##############################################################################
-### EXPERIMENT RUN                                                         ###
-##############################################################################
+    ###########################################################################
+    ### EXPERIMENT RUN                                                      ###
+    ###########################################################################
 
-# Get parameter iterator:
-parameters = get_parameters_dicts(
-    file_split_size=config["file_split_sizes"],
-    segment_size=config["segment_sizes"],
-    thread=config["threads"],
-    core=config["cores"]
-)
-
-# Run experiment on each parameter setting:
-for parameter in parameters:
-    print(f"Run experiment with {parameter} ...")
-
-    experiment = Experiment(
-        config["db"], file, config["version"], config["bucket"],
-        config["cluster"], config["node"], config["tool"],
-        parameter["file_split_size"], parameter["segment_size"],
-        parameter["thread"], parameter["core"]
+    # Get parameter iterator:
+    parameters = get_parameters_dicts(
+        file_split_size=config["file_split_sizes"],
+        segment_size=config["segment_sizes"],
+        thread=config["threads"],
+        core=config["cores"]
     )
 
-    results = experiment.run()
+    # Run experiment on each parameter setting:
+    for parameter in parameters:
+        print(f"Run experiment with {parameter} ...")
 
-    experiment.delete_bucket()
+        #experiment = Experiment(
+        #    config["db"], file, config["version"], config["bucket"],
+        #    config["cluster"], config["node"], config["tool"],
+        #    parameter["file_split_size"], parameter["segment_size"],
+        #    parameter["thread"], parameter["core"]
+        #)
 
-    print("Finished experiment.\n")
+        experiment = SubExperiment(
+            config["db"], file, config["version"], config["bucket"],
+            config["cluster"], config["node"], config["tool"],
+            parameter["file_split_size"], parameter["segment_size"],
+            parameter["thread"], parameter["core"]
+        )
+
+        results = experiment.run()
+
+        experiment.delete_bucket()
+
+        print("Finished experiment.\n")
